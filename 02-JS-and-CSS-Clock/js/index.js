7066dbc2c4e0944af0257a59323ba7d4ccf98343
dykyys <dykyy.s@gmail.com>
2021-10-02 21:55:37 +0300

    add projects

---
+++ 02-JS-and-CSS-Clock/js/index.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector('.hand-second');
+const minHand = document.querySelector('.hand-min');
+const hourHand = document.querySelector('.hand-hour');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const hoursDegrees = (hours / 12) * 360 + 90;
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+setDate();
+setInterval(setDate, 1000);
